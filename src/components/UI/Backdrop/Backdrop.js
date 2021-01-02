0a831946f4186dda2e9c974fbc59dcd90d72e7e2
Andrei <andrews.makarevich@gmail.com>
2021-01-02 20:12:10 +0300

    dev: add Navigation feature, Logo Component
    * Logo Component
    * Navigation feature:
     - NavigationItem Component
     - NavigationItems Component
     - SideDrawer Component incl DrawerToggle Component
     - Toolbar Component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,9 +3,11 @@ import React from 'react';
 import styles from './Backdrop.module.css';
 
 const backdrop = (props) => (
-  props.show ? <div
-    className={styles.Backdrop}
-    onClick={props.clicked} />
+  props.show ?
+    <div
+      className={styles.Backdrop}
+      onClick={props.clicked} />
     : null
-)
+);
+
 export default backdrop;
